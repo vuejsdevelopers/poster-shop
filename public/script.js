4e17326c5e64ce9ab7d5c4fa6313319c5db0a97f
Anthony Gore <anthony@magnum.technology>
2018-11-05 12:16:45 +0700

    .

--- public/script.js
+++ public/script.js
@@ -12,7 +12,12 @@ new Vue({
 	methods: {
 		addToCart: function(product) {
 			this.total += product.price;
-			this.cart.push(product);
+			this.cart.push({
+				id: product.id,
+				title: product.title,
+				price: product.price,
+				qty: 1
+			});
 		}
 	}
 });
