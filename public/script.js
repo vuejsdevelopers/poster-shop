85388f19620827950b19322f0374ff83bf154016
Anthony Gore <anthony@magnum.technology>
2018-11-07 13:44:23 +0700

    .

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+var LOAD_NUM = 4;
+
 new Vue({
 	el: "#app",
 	data: {
@@ -6,7 +8,8 @@ new Vue({
 		cart: [],
 		search: "cat",
 		lastSearch: "",
-		loading: false
+		loading: false,
+		results: []
 	},
 	methods: {
 		addToCart: function(product) {
@@ -41,7 +44,8 @@ new Vue({
 			var path = "/search?q=".concat(this.search);
 			this.$http.get(path)
 				.then(function(response) {
-					this.products = response.body;
+					this.results = response.body;
+					this.products = response.body.slice(0, LOAD_NUM);
 					this.lastSearch = this.search;
 					this.loading = false;
 				});
