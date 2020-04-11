4726c0b233cff8d08457b44d32f6a3610acf80e7
Mark Jackson <Mark.Jackson@southerncross.co.nz>
2020-04-11 22:58:55 +1200

    completed section 4

--- public/script.js
+++ public/script.js
@@ -1,1 +1,23 @@
-console.log("It works!");
+new Vue({
+	el: "#app",
+	data: {
+		total: 0,
+		products: [
+			{
+				title: "Product 1", id: 1
+			},
+			{
+				title: "Product 2", id: 2
+			},
+			{
+				title: "Product 3", id: 3
+			}
+		]
+	},
+	methods: {
+		addToCart: function () {
+			this.total += 9.99
+		}
+	},
+}
+)
